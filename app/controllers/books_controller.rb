49b25d1a30267683566ddfda311ae5289438b843
ruuuh20 <pkim2320@gmail.com>
2018-08-16 10:58:58 -0400

    setup db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,19 @@ class BooksController < ApplicationController
     end
 
     def create
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
     end
 
     def edit
+    end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :description, :author)
     end
 end
