d9a6ae7f8aa5eaceaeb18c65bd8086ad5d47b34a
ruuuh20 <pkim2320@gmail.com>
2018-08-16 11:23:15 -0400

    crud for book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
 
     def index
       @books = Book.all
@@ -20,8 +21,23 @@ class BooksController < ApplicationController
     def edit
     end
 
+    def update
+      if @book.update(book_params)
+        redirect_to book_path(@book)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+    end
+
     private
     def book_params
       params.require(:book).permit(:title, :description, :author)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
     end
 end
